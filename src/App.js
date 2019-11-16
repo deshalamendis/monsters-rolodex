8aaf9e7490ec8354e3b804b778ee88fe98f64f02
Deshala Mendis <deshala@DeshalanoMacBook-Pro.local>
2019-11-16 21:51:08 +0530

    finalzed project

--- src/App.js
+++ src/App.js
@@ -30,8 +30,9 @@ class App extends Component{
 
     return (
       <div className="App">
+        <h1>Monsters Rolodex</h1>
         <SearchBox 
-          placeholder = "search monster"
+          placeholder = "Search Monster"
           handleChange = {this.handleChange}
         />
         <CardList monsters={filteredMonsters}/>
